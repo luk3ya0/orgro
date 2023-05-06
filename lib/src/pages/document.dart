6fab1689856da1c4f27c8c1084e7c1957ba0833e
luk3ya0 <oneTOinf@163.com>
2023-05-07 04:40:15 +0800

    support svg file

--- lib/src/pages/document.dart
+++ lib/src/pages/document.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hybrid_image/hybrid_image.dart';
 import 'package:org_flutter/org_flutter.dart';
 import 'package:orgro/src/actions/actions.dart';
 import 'package:orgro/src/actions/geometry.dart';
@@ -494,6 +497,7 @@ class _DocumentPageState extends State<DocumentPage> with ViewSettingsState {
     if (widget.dataSource.needsToResolveParent) {
       return null;
     }
+
     return LocalImage(
       dataSource: widget.dataSource,
       relativePath: link.body,
--- lib/src/pages/image.dart
+++ lib/src/pages/image.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:orgro/src/data_source.dart';
 import 'package:orgro/src/debug.dart';
 import 'package:orgro/src/navigation.dart';
@@ -75,6 +76,30 @@ class _LocalImageState extends State<LocalImage> {
   }
 
   Widget _futureImage({double scale = 1}) {
+    if (widget.relativePath.endsWith('svg')) {
+      return FutureBuilder<Uint8List?>(
+        future: _bytes,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final rawSvg = String.fromCharCodes(snapshot.data!);
+            return FittedBox(
+              fit: BoxFit.scaleDown,
+              child: SvgPicture.string(rawSvg),
+            );
+          } else if (snapshot.hasError) {
+            return Row(
+              children: [
+                const Icon(Icons.error),
+                const SizedBox(width: 8),
+                Flexible(child: Text(snapshot.error.toString()))
+              ],
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      );
+    }
     return FutureBuilder<Uint8List?>(
       future: _bytes,
       builder: (context, snapshot) {
